47ab85a96359ded4365c44d77d86f702ded7b6c1
Ryan McQuen <Ryan.McQuen@surlatable.com>
2018-01-24 16:26:52 -0800

    Remove site sections.

--- source/site-sections/storeLocator/js/main.js
+++
@@ -1,9 +0,0 @@
-// *****************************************************
-//           INIT CUSTOM PLUGINS
-// *****************************************************
-$(document).ready(function() {});
-
-/***** INIT MAP - called on google callback******/
-function loadMap() {
-	$("#storeMap").getMap();
-}
