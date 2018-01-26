5063354989e6a9628364a481a965f98862312781
Ryan McQuen <Ryan.McQuen@surlatable.com>
2018-01-26 13:52:01 -0800

    Switch to react-habitat-redux (instead of custom ReduxDomFactory).

--- source/styleguide/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Provider } from "react-redux";
-import sltStore from "../store/sltStore";
-
-export default class Wrapper extends React.Component {
-	render() {
-		return <Provider store={sltStore}>{this.props.children}</Provider>;
-	}
-}
